50ee2fe9c8a9fadf6ba25ac4ae38df00530b01f9
Derek Leggo <dleggo@protonmail.com>
2024-12-03 15:39:23 -0700

    Make script better

--- abc.sh
+++ abc.sh
@@ -13,16 +13,30 @@ payload="__payload__$RANDOM.tar"
 [ "$script" != "" ] || read -e -p "Enter the name/path of the script: " script
 
 echo "Compresssing payload..."
-sudo tar -cvf $payload $payload_in || exit 1
-sudo chown $(id -u):$(id -g) $payload
+tar -cvf $payload $payload_in || exit 1
+chown $(id -u):$(id -g) $payload
 
-printf "#!/bin/bash
+cat <<EOF > "$tmp"
+#!/bin/bash
+user_cwd=\$(pwd)
+out=\$(mktemp -d)
+
 PAYLOAD_LINE=\`awk '/^__PAYLOAD_BELOW__/ {print NR + 1; exit 0; }' \$0\`
-tail -n+\$PAYLOAD_LINE \$0 | pv -p -s \$((\$(wc -c < \$0) - \$PAYLOAD_LINE)) | tar -xz
-# you can add custom installation command here
+tail -n+\$PAYLOAD_LINE \$0 | pv -p -s \$((\$(wc -c < \$0) - \$PAYLOAD_LINE)) | tar -xz -C \$out
 
+cd \$out
+####################################
+
+ls
+
+
+
+####################################
+cd \$user_cwd
+rm -rf \$out
 exit 0
-__PAYLOAD_BELOW__\n" > "$tmp"
+__PAYLOAD_BELOW__\n"
+EOF
 
 cat "$tmp" "$payload" > "$script" && rm "$tmp"
 chmod +x "$script"
