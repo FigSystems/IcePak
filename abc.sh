9ea1b5abdd39a7f41b1dfe11a723484eb3e848fa
Derek Leggo <dleggo@protonmail.com>
2024-12-04 21:55:14 -0700

    Add arguments at the top level

--- abc.sh
+++ abc.sh
@@ -4,13 +4,33 @@
 # BASEDIR=`dirname "${0}"`
 # cd "$BASEDIR"
 
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+	-h | --help)
+		echo "Usage: $0 directory output_file"
+		echo "-h, --help      show this help text"
+		shift
+		exit 0
+		;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
 payload_in=$1
 script=$2
 tmp=__extract__$RANDOM
 payload="__payload__$RANDOM.tar"
-
-[ "$payload_in" != "" ] || read -e -p "Enter the path of the directory: " payload_in
-[ "$script" != "" ] || read -e -p "Enter the name/path of the script: " script
 
 echo "Compresssing payload..."
 tar -cvf $payload $payload_in || exit 1
