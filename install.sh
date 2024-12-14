874d553cad49aa870c7fcaa68b5346f77efbccf9
Derek Leggo <dleggo@protonmail.com>
2024-12-13 23:15:51 -0700

    Begin installation script #12

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo cp build-ipak.ipak /usr/bin/build-ipak
+sudo cp -f build-ipak.sh /usr/local/bin/build-ipak
+sudo cp -f dist-to-ipak.sh /usr/local/bin/dist-to-ipak
+sudo cp -f ipak-creater.sh /usr/local/bin/ipak-creater
