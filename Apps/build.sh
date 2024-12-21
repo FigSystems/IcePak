f8642247809e5f8f6e8902f9b172c616f7bf6dd9
Derek Leggo <dleggo@protonmail.com>
2024-12-21 11:17:02 -0700

    Update build.sh

--- Apps/build.sh
+++ Apps/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-for f in *.ipakfile; do
-	build-ipak "$f"
-done
+if [ "$1" == "brave" ]; then
+	build-ipak --distro-file ../build/debian.tgz brave.ipakfile
+else if [ "$1" == "libreoffice" ]; then
+	build-ipak --distro-file ../build/alpine.tgz libreoffice.ipakfile
+else if [ "$1" == "firefox" ]; then
+	build-ipak --distro-file ../build/alpine.tgz firefox.ipakfile
+else if [ "$1" == "gedit" ]; then
+	build-ipak --distro-file ../build/alpine.tgz gedit.ipakfile
+else if [ "$1" == "vscode" ]; then
+	build-ipak --distro-file ../build/debian.tgz vscode.ipakfile
+else
+	for i in *.ipakfile; do
+		build-ipak $i
+	done
+fi
