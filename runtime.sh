4012dffd301297fb99e2e9cd52db792416e67a81
Derek Leggo <dleggo@protonmail.com>
2024-12-28 15:00:17 -0700

    Work on runtime

--- runtime.sh
+++ runtime.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-set -eo pipefail
+SELF="$(readlink -f "$0")"
+SELF_DIR="$(dirname "$SELF")"
+
+trap "exit 1" TERM
+export TOP_PID=$$
 
 function display_error() {
 	if which zenity > /dev/null; then
@@ -25,3 +29,26 @@ function display_warning() {
 		echo "W: $1"
 	fi
 }
+
+function get_config_option() {
+	if [ ! -f "$SELF_DIR/.config/$1" ]; then
+		return 1
+	fi
+
+	cat "$SELF_DIR/.config/$1"
+	return 0
+}
+
+function get_required_config_option() {
+	local VALUE
+	VALUE=$(get_config_option "$1")
+	get_config_option "$1"
+
+	if [ $? != 0 ]; then
+		display_error "Missing required config option: $1. This is a problem with the application. Please contact the application's developer and report this error message to them."
+	fi
+
+	echo "$VALUE"
+}
+
+zenity --info --text="Entrypoint: $(get_required_config_option entrypoint)"
